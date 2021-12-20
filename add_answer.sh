2a3d7b520fa59c365f0d978d28bdd83b011f5bd8
Alexander Tsidaev <a.tsidaev@gmail.com>
2021-12-20 12:01:35 +0500

    Generate answers.json if no such file found

--- add_answer.sh
+++ add_answer.sh
@@ -9,4 +9,16 @@ DAY=`echo $PWD | sed s'/.*\([0-9]\{4\}\)\/0*\([0-9]\{1,2\}\).*/\2/'`
 DAY=$[DAY+1]
 URL=https://adventofcode.com/$PTH
 VAL=`curl $URL -H "Cookie: $COOKIE" 2>/dev/null | grep -o '<p>Your puzzle answer was <code>.*</code>' | grep -o -E '[0-9]+' | tr '\n' ' ' | sed 's/\([0-9]\+\) \([0-9]\+\)/\[ \1, \2\ ]/'`
+
+if [ ! -f ../answers.json ]
+then
+    echo [ >> ../answers.json
+    for i in `seq 1 24` 
+    do
+        echo '    [ ],' >> ../answers.json
+    done
+    echo '    [ ]' >> ../answers.json
+    echo ] >> ../answers.json
+fi
+
 sed -i $DAY's/\[.*\]/'"$VAL"'/' ../answers.json
